04f9a36741d8490e189d1cd0775b77f918f0f220
Lipei Yang <lipei.yang@gmail.com>
2010-07-04 00:39:05 +0800

    5

--- app/models/history_point.rb
+++ app/models/history_point.rb
@@ -1,2 +1,20 @@
 class HistoryPoint < ActiveRecord::Base
+  
+  def time_at_string  
+    time.getlocal.to_s(:db)  
+  end  
+  
+  def time_at_string=(time_at_str)  
+    self.time = Time.parse(time_at_str)  
+  end 
+  
+  def find_history_points(period, region)
+    HistoryPoint.all(:conditions=> [ 
+      "time between :start and :end 
+      and latitude between :east and :west
+      and longitude between :north and :south", 
+      { :start => period[:start], :end => period[:end],
+        :east => region[:east], :west => region[:west],
+        :north => region[:north], :south => region[:south] } ]
+  end
 end
